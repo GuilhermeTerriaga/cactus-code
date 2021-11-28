8dc8d7913aa7950ea43ae3de7850dc35725a3052
Guilherme Terriaga <guilherme.terriaga@gmail.com>
2021-11-28 18:25:13 -0300

    email via email cadastro mais abrangente

--- src/config/mail.js
+++ src/config/mail.js
@@ -5,7 +5,7 @@ export default {
   subjectRecover: 'Recuperação de senha',
   subjectNewUser: 'Bem vindo(a) ao cinefy!',
   mailNewUser:
-    '<p>Bem vindo(a) ao cinefy!</p><figure> <img src="https://www.icegif.com/wp-content/uploads/welcome-icegif-1.gif" alt="Bem vindo(a)!"> <figcaption>Gif com o texto Welcome (bem vindo em inglês)</figcaption> </figure>',
+    '<p>Bem vindo(a) ao cinefy!</p><figure> <img src="https://www.icegif.com/wp-content/uploads/welcome-icegif-1.gif" alt="Bem vindo(a)!"> <figcaption>Gif com o texto Welcome (bem vindo(a) em inglês)</figcaption> </figure>',
   htmlRecover:
     '<p>Por favor, resete sua senha pelo seguinte link: </p> <a href="REPLACE" >recuperar a senha</a><br><p>Se o link não for clicável, por favor copie o seguinte endereço e cole na barra de pesquisa: REPLACE</p>', // plain text body
   link: 'http://localhost:3000/novasenha',
